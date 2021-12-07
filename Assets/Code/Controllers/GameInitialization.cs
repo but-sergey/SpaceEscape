3dc6af71873a625bd19f52a48938e858ff55003d
but-sergey <but-sergey@bk.ru>
2021-12-07 21:17:28 +0300

    Удаляем/комментируем лишнее

--- Assets/Code/Controllers/GameInitialization.cs
+++ Assets/Code/Controllers/GameInitialization.cs
@@ -8,20 +8,19 @@ namespace RollABall
         {
             Camera camera = Camera.main;
 
-            var inputInitialization = new InputInitialization();
-            var playerModel = new PlayerModel(data.Player.Prefab, data.Player.Speed, data.Player.Position, data.Player.Name);
-            var playerFactory = new PlayerFactory(playerModel);
-            var playerInitialization = new PlayerInitialization(playerFactory, playerModel.Position);
+            //var inputInitialization = new InputInitialization();
+            //var playerModel = new PlayerModel(data.Player.Prefab, data.Player.Speed, data.Player.Position, data.Player.Name);
+            //var playerFactory = new PlayerFactory(playerModel);
+            //var playerInitialization = new PlayerInitialization(playerFactory, playerModel.Position);
 
-            controllers.Add(inputInitialization);
-            controllers.Add(playerInitialization);
+            //controllers.Add(inputInitialization);
+            //controllers.Add(playerInitialization);
 
             //controllers.Add(new MapController());
-            controllers.Add(new CameraInitialization(camera.transform, playerModel.Position));
-            controllers.Add(new LevelController(data.Level));
-            controllers.Add(new InputController(playerInitialization.GetPlayer(), inputInitialization.GetInput()));
-            controllers.Add(new MoveController(inputInitialization.GetInput(), playerInitialization.GetPlayer(), playerModel));
-            controllers.Add(new CameraController(playerInitialization.GetPlayer(), camera.transform));
+            //controllers.Add(new CameraInitialization(camera.transform, playerModel.Position));
+            //controllers.Add(new InputController(playerInitialization.GetPlayer(), inputInitialization.GetInput()));
+            //controllers.Add(new MoveController(inputInitialization.GetInput(), playerInitialization.GetPlayer(), playerModel));
+            //controllers.Add(new CameraController(playerInitialization.GetPlayer(), camera.transform));
         }
     }
 }
