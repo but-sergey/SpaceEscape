8df797b7d741ab40c3756ffbd7bc56f04dd3d25b
but-sergey <but-sergey@bk.ru>
2021-12-19 21:07:35 +0300

    Ещё рефакторинг
    Выпилил SaveData (он не нужен пока в этом проекте)

--- Assets/Code/Controllers/InputController.cs
+++ Assets/Code/Controllers/InputController.cs
@@ -6,14 +6,11 @@ namespace SpaceEscape
     {
         private readonly IUserInputProxy _horizontal;
         private readonly IUserInputProxy _vertical;
-        private readonly ISaveDataRepository _saveDataRepository;
         private readonly IFireController _fireController;
         private readonly Transform _player;
 
         public InputController(Transform player, (IUserInputProxy inputHorizontal, IUserInputProxy inputVertical) input, IFireController fireController)
         {
-            _saveDataRepository = new SaveDataRepository();
-
             _player = player;
             _fireController = fireController;
 
@@ -23,16 +20,6 @@ namespace SpaceEscape
 
         public void Execute(float deltaTime)
         {
-            if(Input.GetKeyDown(KeyManager.SAVE))
-            {
-                _saveDataRepository.Save(_player);
-            }
-
-            if (Input.GetKeyDown(KeyManager.LOAD))
-            {
-                _saveDataRepository.Load(_player);
-            }
-
             if (Input.GetKeyDown(KeyManager.FIRE))
             {
                 _fireController.Fire();
--- Assets/Code/SaveData/IData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SpaceEscape
-{
-    public interface IData<T>
-    {
-        void Save(T data, string path = null);
-        T Load(string path = null);
-    }
-}
