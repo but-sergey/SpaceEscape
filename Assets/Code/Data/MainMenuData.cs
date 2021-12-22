419b5d56d03b11f6dc64d5c2279384b7207cc5d5
vilkoivshchi <bugagans@gmail.com>
2021-12-23 00:32:43 +0300

    stored UI elements to ScriptableObject

--- Assets/Code/Data/MainMenuData.cs
+++ Assets/Code/Data/MainMenuData.cs
@@ -1,26 +1,25 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 namespace SpaceEscape
 {
     [CreateAssetMenu(fileName = "MainMenuData", menuName = "Data/Main menu data")]
     public sealed class MainMenuData : ScriptableObject
     {
-        [SerializeField] private Button _continueButton;
-        [SerializeField] private Button _newGameButton;
-        [SerializeField] private GameObject _mainMenu;
+        public Canvas MainMenuCanvas;
+        public Image MainMenuBackgroundImage;
+        public Button NewGameButton;
+        public TextMeshProUGUI NewGameButtonText;
+        public Button ContinueButton;
+        public TextMeshProUGUI ContinueButtonText;
 
+        public GameObject OSDRoot;
+        public TextMeshProUGUI ScoreLabel;
+        public TextMeshProUGUI ScoreCounter;
 
-        public Button ContinueButton
-        {
-            get
-            {
-                return _continueButton;
-            }
-            set
-            {
-                _continueButton = value;
-            }
-        }
+        public TextMeshProUGUI PlayerHealthLabel;
+        public TextMeshProUGUI PlayerHealthCounter;
+
     }
 }
